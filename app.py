ae116cf372af2722531a29885f54dbf8e119ac1b
zachang <zachangdawuda@gmail.com>
2018-06-21 16:25:50 +0100

    CRUD of store app using list

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -9,24 +9,40 @@ stores = [
             {
                 'name': 'Bread',
                 'price': 3.00
-            },
-            {
-                'name': 'Biscuits',
-                'price': 10.00
-            },
+            }
         ]
     }
 ]
 
+@app.route('/')
+def index():
+    return 'Welcome to store'
+
 @app.route('/store', methods=['POST'])
 def add_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+
+    stores.append(new_store)
+    return jsonify(stores)
 
 
-@app.route('/store/<string:name>/item', methods=['POST'])
+@app.route('/store/<string:name>/item', methods=['PUT'])
 def add_store_item(name):
-    pass
-
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            store['items'].append(new_item)
+            return jsonify(store)
+        return jsonify({'message': 'store not found'})
+    
 
 @app.route('/store')
 def retrieve_stores():
@@ -35,12 +51,18 @@ def retrieve_stores():
 
 @app.route('/store/<string:name>')
 def retrieve_store(name):
-    pass
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+    return jsonify({'message': 'store not found'})
 
 
 @app.route('/store/<string:name>/item')
 def retrieve_store_items(name):
-    pass
+    for store in stores:
+        if store['name'] == name:
+            return jsonify({'items': store['items']})
+    return jsonify({'message': 'store not found'})
 
 
 
